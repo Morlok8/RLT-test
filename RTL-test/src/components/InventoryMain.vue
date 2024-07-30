5d4d0dba41bb61781614004655be666e64486f26
Morlok8~ <maik3786@gmail.com>
2024-07-30 19:45:05 +0300

    initializing project

---
+++ RTL-test/src/components/InventoryMain.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class = "main-window">
+        <div class = "side-block">
+        </div>
+        <div class = "inventory">
+
+        </div>
+    </div>
+</template>
+
+<style>
+.main-window{
+    display: flex;
+}
+.side-block{
+    width: 25vw;
+    height: 65vh; 
+    background-color: #4D4D4D;
+    border-radius: 15px;
+    margin-right: 23px;
+}
+.inventory{
+    width: 65vw;
+    height: 65vh; 
+    margin: auto;
+    background-color: #4D4D4D;
+    border-radius: 15px;
+}
+</style>
